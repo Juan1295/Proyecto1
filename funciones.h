c8a06d2ceae8079c43182a20c418195488133f09
Alejandro Ruiz Vallejo <aruizv215@gmail.com>
2015-08-21 20:51:47 -0500

    Se definen las funciones AND EOR MOV ORR SUB

---
+++ funciones.h
@@ -0,0 +1,10 @@
+
+void AND(long int *rx,long int rn,long int rm); //Se define la función AND
+
+void ORR(long int *rx,long int rn,long int rm);//Se define la función OR
+
+void EOR(long int *rx,long int rn,long int rm);//Se define la función EXOR
+
+void MOV(long int *rx,long int rn);//Se define la función MOVER
+
+void SUB(long int *rx,long int rn,long int rm);//Se define la funcion Resta
