29d80f6bd8e9a3b81a952f0a1fce5bfc34c9c3ff
Juan1295 <jmhoyosa_1@uqvirtual.edu.co>
2015-08-28 18:37:16 -0500

    Se agrega la libreria para las banderas y incluyen variables tipo Uint32

--- funciones.h
+++ funciones.h
@@ -1,10 +1,51 @@
+#include<stdint.h>
+#include "flags.h"
 
-void AND(long int *rx,long int rn,long int rm); //Se define la función AND
+/**
+* \brief Funcion logica AND para dos registros .
+* \param rn Primer registro.
+* \param rm Segundo registro.
+* \param *rx Puntero hacia la dirección RX la operación AND.
+* \param banderas Puntero a estructura tipo flg.
+* \return No retorna.
+*/
+void AND(uint32_t *rx,uint32_t rn,uint32_t rm,struct flg *banderas);
 
-void ORR(long int *rx,long int rn,long int rm);//Se define la función OR
+/**
+* \brief Funcion logica ORR para dos registros .
+* \param rn Primer registro.
+* \param rm Segundo registro.
+* \param *rx Direccion donde se almacena la operacion OR.
+* \param *banderas Puntero a estructura tipo flg.
+* \return No retorna.
+*/
+void ORR(uint32_t *rx,uint32_t rn,uint32_t  rm,struct flg *banderas);
 
-void EOR(long int *rx,long int rn,long int rm);//Se define la función EXOR
+/**
+* \brief Funcion logica EOR para dos registros .
+* \param rn Primer registro.
+* \param rm Segundo registro.
+* \param *rx Direccion de memoria donde se almacena la operacion EXOR.
+* \param banderas Puntero a estructura tipo flg.
+* \return No hay retorno.
+*/
+void EOR(uint32_t *rx, uint32_t rn,uint32_t rm,struct flg *banderas);
 
-void MOV(long int *rx,long int rn);//Se define la función MOVER
+/**
+* \brief Funcion logica MOV para de un registro a otro.
+* \param rn Registro a copiar.
+* \param *rx Direccion de memoria donde se almacena la operacion MOVER.
+* \param banderas Puntero a estructura tipo flg.
+* \return No hay retorno.
+*/
+void MOV(uint32_t *rx,uint32_t rn,struct flg *banderas);
 
-void SUB(long int *rx,long int rn,long int rm);//Se define la funcion Resta
+/**
+* \brief Funcion logica SUB para hacer una resta entre 2 registros.
+* \param rn Primer registro.
+* \param rm Segundo registro.
+* \param *rx Direccion de memoria donde se almacena la operacion SUB.
+* \param banderas Puntero a estructura tipo flg.
+* \return No hay retorno.
+*/
+void SUB(uint32_t *rx,uint32_t rn,uint32_t rm,struct flg *banderas);
