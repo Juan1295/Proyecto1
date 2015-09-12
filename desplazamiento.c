4b8e07b9af7005db7baa986cbec88709b076e95c
Alejandro Ruiz Vallejo <aruizv215@gmail.com>
2015-09-11 19:45:57 -0500

    Se modificaron la libreria y las funciones de desplazamiento.c y desplazamiento.h para eliminar la libreria math.h y hacer mas eficiente el codigo

--- desplazamiento.c
+++ desplazamiento.c
@@ -5,19 +5,19 @@
 
 void LSL(uint32_t *rx,uint32_t ra)
 {
-    *rx=*rx*pow(2,ra);//Operacion necesaria para desplazar a la izquierda.
+    *rx=*rx<<ra;//Operacion necesaria para desplazar a la izquierda.
 }
 
 void LSR(uint32_t *rx,uint32_t ra)
 {
-    *rx=*rx/pow(2,ra);//Operacion necesaria para desplazar a la derecha.
+    *rx=*rx>>ra;//Operacion necesaria para desplazar a la derecha.
 }
 
 void ROR(uint32_t *rx, uint32_t ra)
 {
   uint32_t aux1,aux2;//Variables auxiliares para la rotacion
-  aux1=*rx/pow(2,ra);//Se guarda un corrimiento a la derecha en aux1 de *rx
-  aux2=*rx*pow(2,(32-ra));//Se guarda un corrimiento a la izquierda en aux 2 de *rx
+  aux1=*rx>>ra;//Se guarda un corrimiento a la derecha en aux1 de *rx
+  aux2=*rx<<(32-ra);//Se guarda un corrimiento a la izquierda en aux 2 de *rx
   *rx=aux1|aux2;//Se suman las aux para realizar la rotacion
 }
 
@@ -25,7 +25,7 @@ void ASR(uint32_t *rx,uint32_t ra)
 {
     uint32_t aux;//Se define una variable auxiliar.
     aux=*rx&(1<<31);//Se guarda en el auxiliar el valor del bit mas significativo.
-    *rx=*rx/pow(2,ra);//Se guarda en el registro el corrimiento pedido.
+    *rx=*rx>>ra;//Se guarda en el registro el corrimiento pedido.
     *rx=*rx|aux;//Se adiere de nuevo el signo.
 }
 
@@ -60,7 +60,7 @@ void REV(uint32_t *rx,uint32_t ra)
         aux1=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux1;//Se suman logicamente los datos hallados entre 24 y 31 bits en aux.
     }
-    aux1=aux/pow(2,24);//Se corre el bloque de 24 a 31 bits a las posiciones de 0 a 7 y se guarda en aux1.
+    aux1=aux>>24;//Se corre el bloque de 24 a 31 bits a las posiciones de 0 a 7 y se guarda en aux1.
     aux=0;//Inicializo aux en cero para no contar con basura.
     //Segundo bloque que guarda los bits de ra situados entre el bit 0 y 7.
     for(i=0;i<8;i++)
@@ -68,7 +68,7 @@ void REV(uint32_t *rx,uint32_t ra)
         aux2=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux2;//Se suman logicamente los datos hallados entre 24 y 31 bits en aux.
     }
-    aux2=aux*pow(2,24);//Se corre el bloque de 0 a 7 bits a las posiciones de 24 a 31 y se guarda en aux2.
+    aux2=aux<<24;//Se corre el bloque de 0 a 7 bits a las posiciones de 24 a 31 y se guarda en aux2.
     aux=0;//Inicializo aux en cero para no contar con basura.
     //Tercer bloque que guarda los bits de ra situados entre el bit 8 y 15.
     for(i=8;i<16;i++)
@@ -76,7 +76,7 @@ void REV(uint32_t *rx,uint32_t ra)
         aux3=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux3;//Se suman logicamente los datos hallados entre 8 y 15 bits en aux.
     }
-    aux3=aux*pow(2,8);//Se corre el bloque de 8 a 15 bits a las posiciones de 16 a 23 y se guarda en aux3.
+    aux3=aux<<8;//Se corre el bloque de 8 a 15 bits a las posiciones de 16 a 23 y se guarda en aux3.
     aux=0;//Inicializo aux en cero para no contar con basura.
     //Cuarto bloque que guarda los bits de ra situados entre el bit 16 y 23.
     for(i=16;i<24;i++)
@@ -84,7 +84,7 @@ void REV(uint32_t *rx,uint32_t ra)
         aux4=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux4;//Se suman logicamente los datos hallados entre 16 y 23 bits en aux.
     }
-    aux4=aux/pow(2,8);//Se corre el bloque de 16 a 23 bits a las posiciones de 8 a 15 y se guarda en aux4.
+    aux4=aux>>8;//Se corre el bloque de 16 a 23 bits a las posiciones de 8 a 15 y se guarda en aux4.
     //Se guarda en *rx el nuevo dato ordenado.
     *rx=aux1|aux2|aux3|aux4;//Se efectua una or para acomodar cada byte en un solo registro y guardarlo.
 }
@@ -100,7 +100,7 @@ void REV16(uint32_t *rx,uint32_t ra)
         aux1=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux1;//Se suman logicamente los datos hallados entre 0 y 15 bits en aux.
     }
-    aux1=aux*pow(2,16);//Se corre el bloque de 0 a 15 bits a las posiciones de 16 a 31 y se guarda en aux1.
+    aux1=aux<<16;//Se corre el bloque de 0 a 15 bits a las posiciones de 16 a 31 y se guarda en aux1.
     aux=0;//Inicializo aux en cero para no contar con basura.
     //Segundo bloque que guarda los bits de ra situados entre el bit 16 y 31.
     for(i=16;i<32;i++)
@@ -108,7 +108,7 @@ void REV16(uint32_t *rx,uint32_t ra)
         aux2=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux2;//Se suman logicamente los datos hallados entre 16 y 32 bits en aux.
     }
-    aux2=aux/pow(2,16);//Se corre el bloque de 16 a 31 bits a las posiciones de 0 a 15 y se guarda en aux2.
+    aux2=aux>>16;//Se corre el bloque de 16 a 31 bits a las posiciones de 0 a 15 y se guarda en aux2.
     //Se guarda en *rx el nuevo dato ordenado.
     *rx=aux1|aux2;//Se efectua una or para acomodar cada medio registro en uno solo.
 }
@@ -124,7 +124,7 @@ void REVSH(uint32_t *rx,uint32_t ra)
         aux1=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux1;//Se suman logicamente los datos hallados entre 8 y 15 bits en aux.
     }
-    aux1=aux/pow(2,8);//Se corre el bloque de 8 a 15 bits a las posiciones de 0 a 7 y se guarda en aux1.
+    aux1=aux>>8;//Se corre el bloque de 8 a 15 bits a las posiciones de 0 a 7 y se guarda en aux1.
     aux=0;//Inicializo aux en cero para no contar con basura.
     //Segundo bloque que guarda los bits de ra situados entre el bit 0 y 7.
     for(i=0;i<8;i++)
@@ -132,7 +132,7 @@ void REVSH(uint32_t *rx,uint32_t ra)
         aux2=ra&(1<<i);//AND para guardar el bit encontrado en la posicion i.
         aux=aux|aux2;//Se suman logicamente los datos hallados entre 0 y 7 bits en aux.
     }
-    aux2=aux*pow(2,8);//Se corre el bloque de 0 a 7 bits a las posiciones de 8 a 15 y se guarda en aux2.
+    aux2=aux<<8;//Se corre el bloque de 0 a 7 bits a las posiciones de 8 a 15 y se guarda en aux2.
     aux=0;//Inicializo aux en cero para no contar con basura.
     //Extension de signo negativo de manera logica.
     for(i=16;i<32;i++)
